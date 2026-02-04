377527f8d1f5f52298375fd522291dabfebf314e
selin <selin@localhost>
2026-02-04 14:47:55 +0300

    Add basic variables example

---
+++ javaBasicsPractice/src/variables/Variables.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Variables {
+	public static void main(String[] args) {
+		int age = 20; 
+		double height = 1.62; 
+		String name = "Selin";
+		boolean isStudent = true;
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age); 
+		System.out.println("Height: " + height); 
+		System.out.println("Is student: " + isStudent); 
+		} 
+	}
+
+
