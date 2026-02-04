c6f4eb3dd0d2cab4856de5f631f6cafd75ede677
selin <selin@localhost>
2026-02-04 17:40:12 +0300

    Add basic if-else example

---
+++ javaBasicsPractice/src/conditions/IfElseExample.java
@@ -0,0 +1,24 @@
+package conditions;
+
+public class IfElseExample {
+
+	public static void main(String[] args) {
+		
+
+		   
+
+		        int age = 21;
+
+		        if (age >= 18) {
+		            System.out.println("You are an adult.");
+		        } else {
+		            System.out.println("You are not an adult.");
+		        }
+
+		    }
+		
+
+
+	}
+
+
